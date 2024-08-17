83abbc5f0568053038f32151581b0e5f6e69c71b
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-17 16:52:42 +0930

    Create Musician.cpp

---
+++ Musician.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+class Musician {
+private:
+  int year;
+  string instrument;
+
+public:
+  Musician() : year(0), instrument("null"){};
+  Musician(std::string instrument, int experience)
+      : year(experience), instrument(instrument){};
+  std::string get_instrument() { return instrument; }
+  int get_experience() { return year; }
+};
